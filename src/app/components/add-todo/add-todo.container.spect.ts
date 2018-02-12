f4d1a6b04ce435c2b068be0af3d0f05dd5ac13f7
Uladzimir_Panstsiukh <uladzimir.panstsuikhou@gmail.com>
2018-02-12 17:43:58 +0300

    test for add todo component

--- src/app/components/add-todo/add-todo.container.spect.ts
+++ src/app/components/add-todo/add-todo.container.spect.ts
@@ -1,8 +1,15 @@
 import * as angular from 'angular';
 import 'angular-mocks';
 import { AddTodoContainer } from './add-todo.container';
+import { IRootScopeService } from 'angular';
+
 
 describe('AddTodoForm component', () => {
+
+
+    let ctrl:any;
+    let $q: angular.IQService;
+    let scope: IRootScopeService;
 
     let _todoService = {
         add: jasmine.createSpy('add')
@@ -13,17 +20,47 @@ describe('AddTodoForm component', () => {
     };
 
 
-    beforeEach(() => {
-        angular
-            .module('app', [])
-            .component(AddTodoContainer.selector, AddTodoContainer)
-            .value('todoService', _todoService)
-            .value('layoutService', _layoutService);
-        angular.mock.module('app');
+    beforeEach(()=>{
+        angular.mock.module('application.todos');
+    })
+
+    beforeEach((inject(($componentController: any, _$q_:angular.IQService, _$rootScope_:IRootScopeService)=>{
+      
+        ctrl = $componentController(AddTodoContainer.selector, {}, {
+            fetchData: ()=>{}
+        });
+        $q = _$q_;
+        scope = _$rootScope_.$new();
+
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 6000;
+        jasmine.clock().install();
+    })));
+
+    afterEach(function() {
+        jasmine.clock().uninstall();
     });
 
     it('should exist', angular.mock.inject(($componentController: any) => {
-        const component = $componentController(AddTodoContainer.selector, {}, {});
-        expect(component).toBeDefined();
+        expect(ctrl).toBeDefined();
     }));
+
+    it('should call `layoutService.closeAddTodoForm()` by method `closeForm`', ()=>{
+        spyOn(ctrl.layoutService, 'closeAddTodoForm');
+        ctrl.closeForm();
+        expect(ctrl.layoutService.closeAddTodoForm).toHaveBeenCalled();
+    });
+
+    it('should add new todo', (done)=>{
+        const testTodo = {name: 'test', body: 'test', categories: ['test'], urgent: false};
+        spyOn(ctrl.todoService, 'add').and.returnValue($q.resolve());
+        spyOn(ctrl, 'fetchData');
+        ctrl.add(testTodo).then(()=>{
+            expect(ctrl.todoService.add).toHaveBeenCalledWith(testTodo);
+            expect(ctrl.fetchData).toHaveBeenCalled();
+
+            done();
+        });
+        jasmine.clock().tick(6000);
+        scope.$digest();
+    });
 });
